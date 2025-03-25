fed1568003b3407641d1454aa2761829b10204c5
Lsasko <luka.sasko123@gmail.com>
2025-03-25 11:23:49 +0100

    Added Autofac and AutoMapper, implemented DIConfig.cs and AutoMapperProfile.cs Also added .Tests projects for Repository,Service and WebApi

--- Backend/VehicleApp.WebApi/DIConfig.cs
+++ Backend/VehicleApp.WebApi/DIConfig.cs
@@ -1,6 +1,31 @@
 ﻿namespace VehicleApp.WebApi
 {
-    public class DIConfig
+    using Autofac;
+    using AutoMapper;
+
+
+    public class DIConfig : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Registracija repozitorija
+            //builder.RegisterType<>().As<>();
+
+            // Registracija servisa
+            //builder.RegisterType<>().As<>();
+
+            // Registracija AutoMapper-a
+            builder.Register(context => new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AutoMapperProfile>();
+            })).AsSelf().SingleInstance();
+
+            builder.Register(context =>
+            {
+                var scope = context.Resolve<ILifetimeScope>();
+                return new Mapper(scope.Resolve<MapperConfiguration>(), scope.Resolve);
+            }).As<IMapper>().InstancePerLifetimeScope();
+        }
     }
+
 }
