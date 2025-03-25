d7f77e20867249e98870d43f2148c80f8de3816c
Lsasko <luka.sasko123@gmail.com>
2025-03-25 11:25:29 +0100

    Rebuild Project

--- Backend/VehicleApp.WebApi/obj/Debug/net8.0/VehicleApp.WebApi.AssemblyInfo.cs
+++ Backend/VehicleApp.WebApi/obj/Debug/net8.0/VehicleApp.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleApp.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2322f3fae63b999e9eccc11d66cd2167b1463e3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleApp.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleApp.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
