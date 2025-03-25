1331d3d9983ff001db0f5feab67abb7a2be30781
Lsasko <luka.sasko123@gmail.com>
2025-03-25 18:08:41 +0100

    Implemented GenericRepository<T> and IGenericRepository<T> interface

---
+++ Backend/VehicleApp.Repository/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using VehicleApp.Repository.Common;
+
+namespace VehicleApp.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+        public async Task Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+    }
+}
